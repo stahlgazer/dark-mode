53c5bf996a8da9879ce25cc0bfa391790aca4b08
gavin stahl <stahlgavin@gmail.com>
2020-01-15 15:50:24 -0800

    files and local storage setup

---
+++ src/hooks/useLocalStorage.js
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+export const useLocalStorage = (key, initialValue) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    const item = window.localStorage.getItem(key);
+    return item ? JSON.parse(item) : initialValue;
+  });
+  const setValue = value => {
+    // Save state
+    setStoredValue(value);
+    // Save to local storage
+    window.localStorage.setItem(key, JSON.stringify(value));
+  };
+
+  return [storedValue, setValue];
+};
+
+// import { useState } from "react";
+
+// export const useLocalStorage = (key, initialValue) => {
+//     const [storedValue, setStoredValue] = useState(
+//         () => {
+//             if (localStorage.getItem(key)){
+//                 return JSON.parse(localStorage.getItem(key))
+//             }
+//             else {
+//                 localStorage.setItem(key, JSON.stringify(initialValue));
+//                 return initialValue;
+//             }
+//         })
+
+//         const setValue = newValue => {
+//             setStoredValue(newValue);
+//             localStorage.setItem(key, JSON.stringify(newValue));
+//         };
+
+//         return [storedValue, setValue]
+// }
